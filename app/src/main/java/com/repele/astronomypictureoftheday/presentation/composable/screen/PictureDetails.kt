d4e9cf8843516a300677dfe88bc29e0975131f08
Julien <julien@MacBook-Pro-4.local>
2021-12-13 22:07:19 +0100

    Remove unused imports

--- app/src/main/java/com/repele/astronomypictureoftheday/presentation/composable/screen/PictureDetails.kt
+++ app/src/main/java/com/repele/astronomypictureoftheday/presentation/composable/screen/PictureDetails.kt
@@ -1,16 +1,13 @@
 package com.repele.astronomypictureoftheday.presentation.composable.screen
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,22 +15,13 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.annotation.ExperimentalCoilApi
-import coil.compose.ImagePainter.State
-import coil.compose.ImagePainter.State.Empty
-import coil.compose.ImagePainter.State.Error
-import coil.compose.ImagePainter.State.Loading
-import coil.compose.ImagePainter.State.Success
-import coil.compose.rememberImagePainter
 import com.repele.astronomypictureoftheday.presentation.PictureDetailViewModel
-import com.repele.astronomypictureoftheday.presentation.composable.element.Placeholder
 import com.repele.astronomypictureoftheday.presentation.composable.element.Chips
 import com.repele.astronomypictureoftheday.presentation.composable.element.LoadingImage
 
