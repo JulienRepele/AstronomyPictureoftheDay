e1faf9b12d48abda18e47768d180ade41a426300
Julien Repele <j.repele@gmail.com>
2022-09-16 14:49:33 +0200

    Use Coil SubcomposeAsyncImage for the LoadableImage

--- app/src/main/java/com/repele/astronomypictureoftheday/presentation/composable/element/LoadableImage.kt
+++ app/src/main/java/com/repele/astronomypictureoftheday/presentation/composable/element/LoadableImage.kt
@@ -1,8 +1,7 @@
 package com.repele.astronomypictureoftheday.presentation.composable.element
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -10,41 +9,45 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImagePainter
-import coil.compose.rememberAsyncImagePainter
-import coil.request.ImageRequest
+import coil.compose.SubcomposeAsyncImage
+import coil.compose.SubcomposeAsyncImageContent
 
 @Composable
 fun LoadableImage(
     imageUrl: String?,
     modifier: Modifier = Modifier,
 ) {
-    Box(
+    SubcomposeAsyncImage(
+        model = imageUrl,
         modifier = modifier,
+        contentDescription = "",
     ) {
-        val painter = rememberAsyncImagePainter(ImageRequest.Builder(LocalContext.current).data(data = imageUrl).build())
-        Image(
-            painter = painter,
-            contentDescription = null,
-            modifier = Modifier.fillMaxSize(),
-            contentScale = ContentScale.Crop,
-        )
         when (painter.state) {
             is AsyncImagePainter.State.Loading -> {
                 CircularProgressIndicator(
-                    modifier = Modifier.align(Alignment.Center),
-                    color = MaterialTheme.colorScheme.onSurface,
+                    modifier = Modifier.size(20.dp).align(Alignment.Center),
                 )
             }
             is AsyncImagePainter.State.Error -> {
-                Text(
-                    text = "An error has occurred",
-                    color = MaterialTheme.colorScheme.error,
-                    modifier = Modifier.align(Alignment.Center),
+                ErrorText(modifier = Modifier.align(Alignment.Center))
+            }
+            else -> {
+                SubcomposeAsyncImageContent(
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop,
                 )
             }
-           else -> {}
         }
     }
+}
+
+@Composable
+fun ErrorText(modifier: Modifier = Modifier) {
+    Text(
+        text = "An error has occurred",
+        color = MaterialTheme.colorScheme.error,
+        modifier = modifier,
+    )
 }
